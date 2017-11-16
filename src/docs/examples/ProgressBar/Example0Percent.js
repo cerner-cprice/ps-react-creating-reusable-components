03cfc4fe131acbcd3ee53c1b140b17fd6edcb2ba
Cody Price <Cody.Price@cerner.com>
2017-11-16 17:32:25 -0600

    mod6: atoms

---
+++ src/docs/examples/ProgressBar/Example0Percent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ProgressBar from 'ps-react/ProgressBar';
+
+/** 0% progress */
+export default function Example0Percent() {
+  return <ProgressBar percent={0} width={150} />
+}
---
+++ src/docs/examples/ProgressBar/Example75Percent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ProgressBar from 'ps-react/ProgressBar';
+
+/** 75% progress */
+export default function Example75Percent() {
+  return <ProgressBar percent={75} width={150} />
+}
